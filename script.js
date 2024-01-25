051a269266a7bd2e647a9f8ff5fe320a241149e0
Ramkrishna Roy <76205677+Ramkrishna307@users.noreply.github.com>
2024-01-25 10:07:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 //your JS code here. If required.
 function allMethods() {
   //write your code here
+	  const methodNames = Object.getOwnPropertyNames(Math)
+    .filter(item => typeof Math[item] === 'function')
+    .join(', ');
+
+  return methodNames;
 }
 
 alert(allMethods());
